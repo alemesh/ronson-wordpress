7add046e71375ecb3a4c001b5c94e23ce4cda8eb
alemesh <alemesh@acceptic.com>
2018-07-11 18:20:45 +0300

    added about page

---
+++ wp-content/themes/ronson/about-page.php
@@ -0,0 +1,179 @@
+<?php
+/**Template name: about_page */
+get_header();
+//$why_page = get_fields();
+//$post_id = get_the_ID();
+?>
+
+
+        <div class="main">
+            <div class="why-page about" style="background-image: url(<?php bloginfo('template_url')?>/img/blog/why-bg2.png)">
+                <!--<div class="corner-bg"></div>-->
+                <!--<div class="layer-bg" style="background-image: url(img/bg-layer2.png)"></div>-->
+                <!--<div class="layer-bg3" style="background-image: url(img/blog/bg-layer4-4.png)"></div>-->
+                <div class="layer-bg2"></div>
+                <div class="layer-red" style="background-image: url(<?php bloginfo('template_url')?>/img/about/bg-lier2.png)"></div>
+                <!--<div class="layer-bg4-4-4" style="background-image: url(img/blog/bg-layer4-4-4.png)"></div>-->
+
+                <div class="main-holder">
+
+                    <div class="text-section">
+                        <h1>אודות רונסון</h1>
+
+
+                        <div class="content">
+                            <p>"רונסון" היא אחת מחברות יזמות הנדל"ן הגדולות והמובילות בפולין. לחברה ניסיון של 20 שנה בשוק הנדל"ן הפולני, יסודות חזקים, מאזנים מעולים ודוחות רווח והפסד מצויינים, בעשר השנים האחרונות רונסון נסחרת בבורסה הפולנית.</p>
+                            <p>בעלת השליטה ברונסון הינה קבוצת עמוס לוזון יזמות ואנרגיה בע"מ, שבבעלותה עשרות פרויקטים בארץ ובעולם, והיא נחשבת לאחת הקבוצות המובילות בשוק הנדל"ן הישראלי.</p>
+                            <p>לרונסון <b>עשרות פרויקטים שהקמתם הושלמה ועוד כעשרה פרויקטים הנמצאים כרגע בתהליכי שיווק ובנייה</b> והיא החברה היחידה בפולין שמיוצגת על ידי סניף שיווק ומכירות בישראל.</p>
+                            <p>"רונסון" זכתה בפרסים רבים וציונים לשבח, ודורגה ע"י איגוד ה-SEG והאיגוד הפולני של החברות הנסחרות כמובילה בנושאים של:</p>
+                        </div>
+                    </div>
+
+                </div>
+
+                <div class="icons-block">
+                    <div class="main-holder">
+                        <ul>
+                            <li>
+                                <div class="wrap-img">
+                                    <img src="<?php bloginfo('template_url')?>/img/about/Rectangle8.png" alt="">
+                                </div>
+                                <div class="wrap-text">
+                                    <p>תקשורת עם משקיעים</p>
+                                </div>
+
+                            </li>
+                            <li>
+                                <div class="wrap-img">
+                                    <img src="<?php bloginfo('template_url')?>/img/about/Rectangle8.png" alt="">
+                                </div>
+                                <div class="wrap-text">
+                                    <p>דעת קהל בקרב משקיעים פרטיים</p>
+                                </div>
+
+                            </li>
+                            <li>
+                                <div class="wrap-img">
+                                    <img src="<?php bloginfo('template_url')?>/img/about/Rectangle8.png" alt="">
+                                </div>
+                                <div class="wrap-text">
+                                    <p>חדשנות ואינטואיטיביות </p>
+                                </div>
+
+                            </li>
+                            <li>
+                                <div class="wrap-img">
+                                    <img src="<?php bloginfo('template_url')?>/img/about/Rectangle8.png" alt="">
+                                </div>
+                                <div class="wrap-text">
+                                    <p>בהירות המסר ותוכן המידע </p>
+                                </div>
+
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="video-block">
+                    <div class="main-holder">
+                        <h3>רונסון מתחדשת כל הזמן:</h3>
+                        <div class="wrap-block">
+                            <div class="icons-left-block">
+                                <ul>
+                                    <li>
+                                        <div class="wrap-img">
+                                            <img src="<?php bloginfo('template_url')?>/img/about/Vector-1.png" alt="">
+                                        </div>
+                                        <div class="wrap-text">
+                                            <p>הפרויקטים החדשים של רונסון משלבים איכות בלתי מתפשרת ברמת הגימור</p>
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <div class="wrap-img">
+                                            <img src="<?php bloginfo('template_url')?>/img/about/Vector-2.png" alt="">
+                                        </div>
+                                        <div class="wrap-text">
+                                            <p>הנכסים מוקמים תוך הקפדה על עיצוב מודרני ופונקציונלי</p>
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <div class="wrap-img">
+                                            <img src="<?php bloginfo('template_url')?>/img/about/Vector-3.png" alt="">
+                                        </div>
+                                        <div class="wrap-text">
+                                            <p>הנכסים אטרקטיביים - ממוקמים בערים מרכזיות </p>
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <div class="wrap-img">
+                                            <img src="<?php bloginfo('template_url')?>/img/about/Vector-4.png" alt="">
+                                        </div>
+                                        <div class="wrap-text">
+                                            <p>תשתית עירונית מקיפה לרבות: קניונים, פארקים, מכוני כושר ומרכזי ספורט </p>
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <div class="wrap-img">
+                                            <img src="<?php bloginfo('template_url')?>/img/about/Vector-5.png" alt="">
+                                        </div>
+                                        <div class="wrap-text">
+                                            <p>תחבורה מתקדמת ונגישות עירונית</p>
+                                        </div>
+                                    </li>
+                                </ul>
+                            </div>
+                            <div class="video" id="video-section">
+                                <!--<video src="img/Ronson_FINAL.mp4"></video>-->
+                                <video class="video-controll mobile-hidden-video" id="videoPlayer" controls poster="<?php bloginfo('template_url')?>/img/video-play.png" onclick="this.paused ? this.play() : this.pause();">
+                                    <!--<source src="video/duel.ogv" type='video/ogg; codecs="theora, vorbis"'>-->
+                                    <source  src="<?php bloginfo('template_url')?>/video/Ronson_FINAL.mp4" type='video/mp4; codecs="avc1.42E01E, mp4a.40.2"'>
+                                    <!--<source src="video/Ronson_FINAL.mp4" type='video/mp4'>-->
+                                    <!--<source src="video/duel.webm" type='video/webm; codecs="vp8, vorbis"'>-->
+                                    <!--<a href="img/Ronson_FINAL.mp4">Скачайте видео</a>-->
+                                </video>
+                                <video class="video-controll desctop-hidden-video" id="videoPlayer" controls poster="<?php bloginfo('template_url')?>/img/video-play.png" onclick="this.paused ? this.play() : this.pause();">
+                                    <!--<source src="video/duel.ogv" type='video/ogg; codecs="theora, vorbis"'>-->
+                                    <source src="<?php bloginfo('template_url')?>/video/ronson.mp4" type='video/mp4; codecs="avc1.42E01E, mp4a.40.2"'>
+                                    <!--<source src="video/Ronson_FINAL.mp4" type='video/mp4'>-->
+                                    <!--<source src="video/duel.webm" type='video/webm; codecs="vp8, vorbis"'>-->
+                                    <!--<a href="img/Ronson_FINAL.mp4">Скачайте видео</a>-->
+                                </video>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+                <div class="black-section">
+                    <div class="main-holder">
+                        <div class="wrap-section">
+                            <div class="left-block">
+                                <p>ליווי אישי וצמוד לאורך התהליך </p>
+                                <p>חבילת שירות כוללת של טיפול מקיף והתנהלות מול הרשויות, לרבות איתור חברה לניהול הנכס וביצוע עבודות גמר</p>
+                                <p>ידע מקיף בזירת הנדל"ן הפולנית</p>
+                            </div>
+                            <div class="right-block">
+                                <div class="wrap-text">
+                                    <p>מלבד קניית הנכס,</p>
+                                    <p>לרונסון ישראל <b>יתרונות נוספים:</b></p>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+
+            </div>
+
+
+
+
+
+
+
+
+
+
+
+
+<?php
+get_footer();
+?>
